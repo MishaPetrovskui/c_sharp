e8e5d27a336ced20757f1da2f57181c5de756a33
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2024-12-02 20:00:25 +0200

    Update class work 2.12.24.cs

--- class work 2.12.24.cs
+++ class work 2.12.24.cs
@@ -3,30 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading
+using Game;
 
-class Character
-{
-    string? name;
-    int health;
-    int damage;
-    int defence;
 
-    public Character(string? _name, int _health, int _damage, int _defence)
-    {
-        this.name = _name;
-        this.health = _health;
-        this.damage = _damage;
-        this.defence = _defence;
-    }
-
-    public void print()
-    {
-        Console.WriteLine($"-< {name} >-");
-        Console.WriteLine($"Health: {health}");
-        Console.WriteLine($"Damage: {damage}");
-        Console.WriteLine($"Defence: {defence}");
-    }
-}
 
 class Proggram
 {
@@ -34,8 +14,66 @@ class Proggram
     {
         Console.OutputEncoding = UTF8Encoding.UTF8;
         Console.InputEncoding = UTF8Encoding.UTF8;
-        Character player1 = new Character("FAKOOR", 100, -100, -1000);
+        Character player1 = new Character("ANTON", 1, 100, 10);
         player1.print();
-        Console.WriteLine($"POPADANIE");
+        Console.WriteLine("\n");
+        Character player2 = new Character("DIMON", 1, 10, 100);
+        player2.print();
+
+        while (true)
+        {
+
+        }
+
+        player1.attack(player2);
+        Console.WriteLine("\n");
+        player1.print();
+        Console.WriteLine("\n");
+        player2.print();
+    }
+}
+
+// DRUGOE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Character
+    {
+        string? name;
+        int health;
+        int damage;
+        int defence;
+
+        public Character(string? _name, int _health, int _damage, int _defence)
+        {
+            this.name = _name;
+            this.health = _health;
+            this.damage = _damage;
+            this.defence = _defence;
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"-< {name} >-");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Damage: {damage}");
+            Console.WriteLine($"Defence: {defence}");
+        }
+
+        public int takeDamage(int damage )
+        {
+            this.health = Math.Max(this.health - damage, 0);
+            return this.health;
+        }
+
+        public void attack(Character target)
+        {
+            target.takeDamage(this.damage);
+        }
     }
 }
